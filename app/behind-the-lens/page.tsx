31346c27d9d2cfda6f39fd98f62ac89c216aa1ed
bangersbreakingfree-dotcom <sawyerkuepker@gmail.com>
2026-07-03 02:19:26 -0500

    Update page.tsx

--- app/behind-the-lens/page.tsx
+++ app/behind-the-lens/page.tsx
@@ -82,9 +82,10 @@ export default async function FounderPage() {
 
       <section className="grid lg:grid-cols-2 border-y border-white/10">
         <div
-  className="min-h-[650px] bg-cover bg-center"
+  className="min-h-[650px] bg-cover bg-no-repeat"
   style={{
     backgroundImage: `url(${content.founder_portrait_url || content.founder_image_url})`,
+    backgroundPosition: "center 20%",
   }}
 />
 
