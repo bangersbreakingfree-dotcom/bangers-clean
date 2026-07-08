83d4893a17d08392f6f48e2bb3c02358bffb4773
bangersbreakingfree-dotcom <sawyerkuepker@gmail.com>
2026-07-07 20:38:34 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -108,7 +108,7 @@ const enrollmentCloses = 'March 15, 2026';
     )}
 
     <a
-  href="#membership"
+  href="/account"
   onClick={() => setMobileMenuOpen(false)}
   className="border border-white/20 px-5 py-2 rounded-xl hover:bg-white hover:text-black transition"
 >
